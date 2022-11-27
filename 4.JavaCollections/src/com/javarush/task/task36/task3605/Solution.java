c6c785348136c01f974857780d61098cef70cdf3
Konstantin Akshentsev <akshentsev69@gmail.com>
2022-11-27 17:38:55 +0300

    Коллекции, 6 уровень, 8 лекция

--- 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
@@ -1,8 +1,8 @@
 package com.javarush.task.task36.task3605;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.TreeSet;
 
 /* 
@@ -11,5 +11,13 @@ import java.util.TreeSet;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+        TreeSet<Character> result = new TreeSet<>();
+            for (byte bts : Files.readAllBytes(Paths.get(args[0]))){
+                if (Character.isLetter((char) bts)){
+                    char next = (char) Character.toLowerCase(bts);
+                    result.add(next);
+                }
+            }
+        result.stream().limit(5).forEach(System.out::print);
     }
 }
