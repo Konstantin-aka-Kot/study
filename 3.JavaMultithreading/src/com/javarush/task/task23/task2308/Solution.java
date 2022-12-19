0b59b2c20ceebe6dc8716bfd8452dba810e6c7ff
Konstantin Akshentsev <akshentsev69@gmail.com>
2022-12-19 20:14:43 +0300

    Multithreading, level 3, lecture 6

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2308/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task23.task2308;
+
+/* 
+Рефакторинг, вложенные классы
+*/
+
+public class Solution {
+    public final static class Constants {
+        public final static String SERVER_IS_CURRENTLY_NOT_ACCESSIBLE = "The server is not currently accessible.";
+        public final static String USER_IS_NOT_AUTHORIZED = "The user is not authorized.";
+        public final static String USER_IS_BANNED = "The user is banned.";
+        public final static String ACCESS_IS_DENIED = "Access is denied.";
+    }
+    public static void main(String[] args) {
+
+    }
+}
