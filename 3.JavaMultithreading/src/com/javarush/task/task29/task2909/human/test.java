95136a32cfe1fc992ea0109b9a30fce1cfc23bad
Konstantin Akshentsev <akshentsev69@gmail.com>
2022-12-26 20:08:08 +0300

    Multithreading, level 5, lecture 15

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/test.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task29.task2909.human;
+
+public class test {
+    public static void main(String[] args) {
+        new Human("Men", 22);
+        new Soldier("Sol", 22);
+        System.out.println("OK");
+    }
+}
